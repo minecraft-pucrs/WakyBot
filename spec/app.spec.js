7ab4ee7bb5ae2169d79360cb47e0d8bd1c714151
Sarah Lacerda <sarah.lacerda@mail.com>
2020-07-28 23:45:52 -0300

    Create sample HelloWorld test file

---
+++ spec/app.spec.js
@@ -0,0 +1,8 @@
+const helloWorld = 'Hello World'
+
+describe('Hello World test', () => {
+
+    it('should call Discord.Client()', () => {
+        expect(helloWorld).toBe("Hello World")
+    });
+});
