cec548374a3e8cd600095c333abab2ffa5eacbbf
Sarah Lacerda <sarah.lacerda@mail.com>
2020-08-03 23:17:23 -0300

    Create MinecraftServerStatusAdapter

---
+++ src/adapter/MinecraftServerStatusAdapter.js
@@ -0,0 +1,25 @@
+const request = require('request');
+const pino = require('pino');
+
+const logger = pino({ level: process.env.LOG_LEVEL || 'info' });
+
+let url;
+
+module.exports = {
+
+  getServerInfo(ip, port = 25565) {
+    logger.info('Fetching Minecraft Server Status...');
+    return new Promise((resolve, reject) => {
+      url = `https://mcapi.us/server/status?ip=${ip}&port=${port}`;
+      request(url, { json: true }, (err, res, body) => {
+        if (err) {
+          logger.err(`Error while fetching Minecraft Server Status ${err}`);
+          reject(err);
+        }
+        logger.info('Successfully fetched Minecraft Server Status');
+        logger.debug(`Returned body: ${body}`);
+        resolve(body);
+      });
+    });
+  },
+};
